9a546e5e58e82a1649face039dc94ca9f273ab9c
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2022-07-26 18:24:54 +0300

    Project modification.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,14 @@
-self.addEventListener("installed", async function(e){})
+self.addEventListener("install", async function (e) {
+  e.waitUntil(self.skipWaiting());
+  console.log("[Service Worker] Installed successfully.");
+});
 
-self.addEventListener("activate", async function(e){})
+self.addEventListener("activate", async function (e) {
+  e.waitUntil(self.clients.claim());
+  console.log("[Service Worker] Activated.");
+});
 
-self.addEventListener("fetch", async function(e){})
+self.addEventListener("fetch", async function (e) {
+  console.log("[Service Worker] Received fetch request.");
+  e.respondWith(fetch(e.request));
+});
